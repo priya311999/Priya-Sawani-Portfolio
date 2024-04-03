44af73dfa47153271d13a466698b531ac562b00c
priya311999 <72311356+priya311999@users.noreply.github.com>
2024-04-03 15:55:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,115 @@
+$(document).ready(function() {
+
+    //sticky header
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 1) {
+          $(".header").addClass("sticky");
+        } else {
+          $(".header").removeClass("sticky");
+        }
+            
+    
+        // Update the active section in the header
+        updateActiveSection();
+      });
+    
+      $(".header ul li a").click(function(e) {
+        e.preventDefault(); 
+    
+        var target = $(this).attr("href");
+    
+        if ($(target).hasClass("active-section")) {
+          return; 
+        }
+    
+        if (target === "#home") {
+          $("html, body").animate(
+            {
+              scrollTop: 0 
+            },
+            500
+          );
+        } else {
+          var offset = $(target).offset().top - 80; 
+    
+          $("html, body").animate(
+            {
+              scrollTop: offset
+            },
+            500
+          );
+        }
+    
+        $(".header ul li a").removeClass("active");
+        $(this).addClass("active");
+      });
+    
+  
+      //Initial content revealing js
+      ScrollReveal({
+        distance: "100px",
+        duration: 2000,
+        delay: 200
+      });
+    
+      ScrollReveal().reveal(".header a, .profilePhoto, .aboutMe, .education", {
+        origin: "left"
+      });
+      ScrollReveal().reveal(".profileText, .skillsContainer, .internship", {
+        origin: "right"
+      });
+      ScrollReveal().reveal(".projectTitle, .contact-title", {
+        origin: "top"
+      });
+      ScrollReveal().reveal(".projects, .contact", {
+        origin: "bottom"
+      });
+  
+    //contact form to excel sheet
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbyTeQNaruiqHGtm8_pE3SdVcPewX34kK028-1TjuTMO4rLNaQ0BGxPnIThksG_pINddKw/exec';
+    const form = document.forms['submitToGoogleSheet']
+    const msg = document.getElementById("msg")
+  
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form),  mode: 'no-cors'})
+            .then(response => {
+                msg.innerHTML = "Message sent successfully"
+                setTimeout(function () {
+                    msg.innerHTML = ""
+                }, 5000)
+                form.reset()
+            })
+            .catch(error => console.error('Error!', error.message))
+    })
+      
+    });
+    
+    function updateActiveSection() {
+      var scrollPosition = $(window).scrollTop();
+    
+      // Checking if scroll position is at the top of the page
+      if (scrollPosition === 0) {
+        $(".header ul li a").removeClass("active");
+        $(".header ul li a[href='#home']").addClass("active");
+        return;
+      }
+    
+      // Iterate through each section and update the active class in the header
+      $("section").each(function() {
+        var target = $(this).attr("id");
+        var offset = $(this).offset().top;
+        var height = $(this).outerHeight();
+    
+        if (
+          scrollPosition >= offset - 40 &&
+          scrollPosition < offset + height - 40
+        ) {
+          $(".header ul li a").removeClass("active");
+          $(".header ul li a[href='#" + target + "']").addClass("active");
+        }
+      });
+    }
+    
+  
+   
